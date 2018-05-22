365d812349ef0a8ff73f008c9ea470c3c32ea08e
weiwen <weiwen@zb100.com>
2018-05-22 11:05:15 +0800

    submit base pkg

---
+++ src/main/java/com/lijingyao/concurrent/base/BaseTest.java
@@ -0,0 +1,7 @@
+package com.lijingyao.concurrent.base;
+
+/**
+ * Created by lijingyao on 2018/5/22 11:04.
+ */
+public class BaseTest {
+}
---
+++ src/main/java/com/lijingyao/concurrent/base/volatiles/VolatileQueue.java
@@ -0,0 +1,11 @@
+package com.lijingyao.concurrent.base.volatiles;
+
+/**
+ * Created by lijingyao on 2018/5/22 11:02.
+ */
+public class VolatileQueue {
+
+    public static void main(String[] args){
+
+    }
+}
